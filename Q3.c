020809f766caa3df3482353f9f996b0c310ef8ec
Joás de Brito Ferreira Filho <appsec.joas@gmail.com>
2025-03-20 13:46:33 -0300

    Create Q3.c
    Write a C/C++ or Java program that prints each number from 1 to 100 on a new line.
    
    For each multiple of 3, print "Foo" instead of the number.
    For each multiple of 5, print "Baa" instead of the number.
    For multiple numbers simultaneously of 3 and 5, print "FooBaa", instead of the number.

---
+++ Q3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int i = 1;
+  for(i=1; i<=100; i++){
+    int mult=0; // multiple verifier: 0 for none, 1 for 3, 2 for 5 and 3 for both
+    if(i%3 == 0){
+      mult = 1;
+    }
+    if(i%5 == 0){
+      mult += 2;
+    }
+    if(mult==0){
+      printf("%d\n", i);
+      continue;
+    }
+    if(mult==1){
+      printf("Foo\n");
+      continue;
+    }
+    if(mult==2){
+      printf("Baa\n");
+    }
+    if(mult==3){
+      printf("FooBaa\n");
+    }
+  }
+}
